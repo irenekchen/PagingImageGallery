a8e882b8ee628cd4806cd94f2221556c6a40b538
Irene <irene.chen@jpl.nasa.gov>
2018-07-31 13:43:20 -0700

    Initial Commit

---
+++ PagingImageGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PagingImageGallery
+//
+//  Created by Chen, Irene (398N-Affiliate) on 7/31/18.
+//  Copyright © 2018 Chen, Irene (398N-Affiliate). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
